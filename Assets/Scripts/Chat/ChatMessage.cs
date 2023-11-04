3ba1e32fe35a18fbabeb4bd76cf206489bf5c5a0
Kan-kun <ndkhang.work@gmail.com>
2023-11-04 13:33:40 +0700

    chat mess

--- Assets/Scripts/Chat/ChatMessage.cs
+++ Assets/Scripts/Chat/ChatMessage.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChatMessage : MonoBehaviour
 {
+    public Image img;
+    public RectTransform rectTrans;
+    Button button;
+    ChatMenu chatMenu;
+    int index;
+    public Text message;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        img = GetComponent<Image>();
+        rectTrans = GetComponent<RectTransform>();
+        chatMenu = rectTrans.parent.GetComponent<ChatMenu>();
+        index = rectTrans.GetSiblingIndex() - 1;
+
+        //add click listener
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnItemClick);
+        message = transform.GetChild(0).GetComponent<Text>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnItemClick()
     {
-        
+        chatMenu.OnItemClick(index);
+    }
+
+    void OnDestroy()
+    {
+        //remove click listener to avoid memory leaks
+        button.onClick.RemoveListener(OnItemClick);
     }
 }
